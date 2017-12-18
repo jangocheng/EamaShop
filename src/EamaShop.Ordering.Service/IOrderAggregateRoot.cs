be40dafe9c5dc1f936916e081ed61e3442676631
亚锋 吴 <wuyafeng909099000@qq.com>
2017-12-18 09:24:55 +0800

    订单服务模块

---
+++ src/EamaShop.Ordering.Service/IOrderAggregateRoot.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EamaShop.Ordering.Service
+{
+    public interface IOrderAggregateRoot
+    {
+        string OrderNumber { get; }
+    }
+}
---
+++ src/EamaShop.Ordering.Service/IOrderFactory.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace EamaShop.Ordering.Service
+{
+    public interface IOrderFactory<TOrder>
+        where TOrder : IOrderAggregateRoot
+    {
+
+    }
+}
