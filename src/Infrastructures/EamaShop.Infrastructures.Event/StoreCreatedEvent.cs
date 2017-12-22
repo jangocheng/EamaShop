ef1a7b2ef33c3df2caa677b8165ecbeafd4e3164
亚锋 吴 <wuyafeng909099000@qq.com>
2017-12-22 13:26:20 +0800

    项目文件修改

---
+++ src/Infrastructures/EamaShop.Infrastructures.Event/StoreCreatedEvent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EamaShop.Infrastructures.Events
+{
+    public class StoreCreatedEvent : IEventMetadata
+    {
+        public long StoreId { get; set; }
+
+        public string StoreName { get; set; }
+
+        public string StoreDescription { get; set; }
+
+        public string StoreLogoUri { get; set; }
+
+        public IEnumerable<string> StoreScopes { get; set; }
+
+        public long UId { get; set; }
+    }
+}
